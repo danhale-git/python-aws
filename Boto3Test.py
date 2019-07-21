19470dd9b81ee34ca233c4cd894b1f0e4e939ce4
DESKTOP-P30ROAU\Salty <danhale87@gmail.com>
2019-07-21 19:11:06 +0100

    removed pprint and move printing to function

--- Boto3Test.py
+++ Boto3Test.py
@@ -1,14 +1,16 @@
 import boto3
-import pprint
+
+def PrintInstanceInformation(instance):
+    print(instance['InstanceId'])
+    print(instance['InstanceType'])
+    print(instance['PublicDnsName'])
+    for nic in instance['NetworkInterfaces']:
+        print(nic['Association']['PublicIp'])
 
 ec2 = boto3.client('ec2')
 
-instances = ec2.describe_instances()
+apiResponse = ec2.describe_instances()
 
-for reservation in instances['Reservations']:
+for reservation in apiResponse['Reservations']:
     for instance in reservation['Instances']:
-        print(instance['InstanceId'])
-        print(instance['InstanceType'])
-        print(instance['PublicDnsName'])
-        for nic in instance['NetworkInterfaces']:
-            print(nic['Association']['PublicIp'])
+        PrintInstanceInformation(instance)
