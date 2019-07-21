9aeea58339c683e710cf7563a97f7b26692515f5
DESKTOP-P30ROAU\Salty <danhale87@gmail.com>
2019-07-21 18:31:49 +0100

    pretty print

--- Boto3Test.py
+++ Boto3Test.py
@@ -5,4 +5,7 @@ ec2 = boto3.client('ec2')
 
 instances = ec2.describe_instances()
 
-print(type(instances))
+PrettyPrint(instances)
+
+def PrettyPrint(data):
+    print(json.dumps(data, indent=4, sort_keys=True))
