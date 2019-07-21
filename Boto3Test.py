11ad57a1383db15ac3c7f480a51cff93c6dbf0ee
DESKTOP-P30ROAU\Salty <danhale87@gmail.com>
2019-07-21 18:32:30 +0100

    function definition before call

--- Boto3Test.py
+++ Boto3Test.py
@@ -1,11 +1,11 @@
 import boto3
 import json
 
+def PrettyPrint(data):
+    print(json.dumps(data, indent=4, sort_keys=True))
+
 ec2 = boto3.client('ec2')
 
 instances = ec2.describe_instances()
 
 PrettyPrint(instances)
-
-def PrettyPrint(data):
-    print(json.dumps(data, indent=4, sort_keys=True))
