36d4b9dd29ba26a548df8973b4fbc0c955a47d02
DESKTOP-P30ROAU\Salty <danhale87@gmail.com>
2019-07-21 19:04:31 +0100

    corrected reservation loop

--- Boto3Test.py
+++ Boto3Test.py
@@ -6,7 +6,7 @@ ec2 = boto3.client('ec2')
 instances = ec2.describe_instances()
 
 for reservation in instances['Reservations']:
-    for instance in reservation:
+    for instance in reservation['Instances']:
         print(instance['InstanceId'])
         print(instance['InstanceType'])
         print(instance['PublicDnsName'])
