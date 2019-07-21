9d2a2cc894b48f6c91b3c37674f5544aceb803d9
DESKTOP-P30ROAU\Salty <danhale87@gmail.com>
2019-07-21 18:53:33 +0100

    removed PrintField()

--- Boto3Test.py
+++ Boto3Test.py
@@ -1,15 +1,13 @@
 import boto3
 import pprint
 
-def PrintField(dictObject, fieldName):
-    print(fieldName+": "+dictObject[fieldName])
-
 ec2 = boto3.client('ec2')
 
 instances = ec2.describe_instances()['Reservations'][0]['Instances']
 
 for instance in instances:
-    PrintField(instance, 'InstanceId')
-    PrintField(instance, 'InstanceType')
+    print(instance['InstanceId'])
+    print(instance['InstanceType'])
+    print(instance['PublicDnsName'])
     for nic in instance['NetworkInterfaces']:
-        PrintField(nic, 'PublicIp')
+        print(instance['Association']['PublicIp'])
