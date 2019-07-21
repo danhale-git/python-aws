2164d080b38ec4400b4c667bd9a2dc6ba6524f2b
DESKTOP-P30ROAU\Salty <danhale87@gmail.com>
2019-07-21 18:51:52 +0100

    missed error

--- Boto3Test.py
+++ Boto3Test.py
@@ -2,7 +2,7 @@ import boto3
 import pprint
 
 def PrintField(dictObject, fieldName):
-    print(fieldName+": "dictObject[fieldName])
+    print(fieldName+": "+dictObject[fieldName])
 
 ec2 = boto3.client('ec2')
 
@@ -11,5 +11,5 @@ instances = ec2.describe_instances()['Reservations'][0]['Instances']
 for instance in instances:
     PrintField(instance, 'InstanceId')
     PrintField(instance, 'InstanceType')
-    for nic in instance['NetworkInterfaces']
+    for nic in instance['NetworkInterfaces']:
         PrintField(nic, 'PublicIp')
