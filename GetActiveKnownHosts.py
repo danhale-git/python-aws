76b84634c05151a48c315b081cae859be0894a93
DESKTOP-P30ROAU\Salty <danhale87@gmail.com>
2019-08-01 18:08:01 +0100

    Rename and TODO

--- GetActiveKnownHosts.py
+++ GetActiveKnownHosts.py
@@ -1,7 +1,17 @@
 import boto3
 import paramiko
 
+#TODO
+# implement argparse for known_hosts path
+# hash new known_hosts contents using paramiko?
+# is there something lighter than ec2.describe_instances that can be used to get ec2 public dns hosts?
+# print actual known_hosts file entries 
+# handle boto3 client errors
+
 known_hostsPath = '/home/dhale/.ssh/known_hosts'
+
+activeInstances = []
+activeHostEntries = []
 
 def GetActiveInstances():
     ec2 = boto3.client('ec2')
@@ -21,9 +31,6 @@ def GetActiveHostEntries():
             activeHostEntries.append(entry)
         else:
             activeInstances.remove(instance)
-
-activeInstances = []
-activeHostEntries = []
 
 GetActiveInstances()
 GetActiveHostEntries()
